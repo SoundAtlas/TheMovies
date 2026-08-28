e35a8acc56d2a221f4e9b5964a000ddc147b330e
Andreas Hemmer <andreas.hemmer@hotmail.com>
2026-08-28 17:55:45 +0200

    Make ViewModelBase an abstract class
    Changed ViewModelBase from a concrete to an abstract class by adding the abstract modifier.

--- TheMovies.WPF/ViewModels/ViewModelBase.cs
+++ TheMovies.WPF/ViewModels/ViewModelBase.cs
@@ -3,7 +3,7 @@ using System.Runtime.CompilerServices;
 
 namespace TheMovies.WPF.ViewModels
 {
-    public class ViewModelBase : INotifyPropertyChanged
+    public abstract class ViewModelBase : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler? PropertyChanged;
 
